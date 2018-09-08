5cdc098cc179cff63b2436271da35f90129b9003
Rafael Alonso <pepe1615@hotmail.com>
2018-09-08 16:49:39 -0300

    balas con rastro y rebote

--- VRShooter/Assets/LaserBullet.cs
+++ VRShooter/Assets/LaserBullet.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class LaserBullet : MonoBehaviour {
     public int damage = 500;
     public int multiplier = 1;
+    public float lifeSpan = 250f;
+    public GameObject bulletMark;
 
     private void Start()
     {
-        Destroy(gameObject, 222f);
+        Destroy(gameObject, lifeSpan);
     }
 
     public int GetDamage()
