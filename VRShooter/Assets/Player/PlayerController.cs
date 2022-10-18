073522190148165e89cab7f7342014b2257414c7
Rafael Alonso <pepe1615@hotmail.com>
2022-10-18 14:59:26 -0300

    Update PlayerController.cs

--- VRShooter/Assets/Player/PlayerController.cs
+++ VRShooter/Assets/Player/PlayerController.cs
@@ -8,65 +8,21 @@ public class PlayerController : MonoBehaviour
 
     public GameObject heldWeapon;
     public SteamVR_TrackedObject trackedObject;
-   // protected FixedJoint joint;
-    //protected Rigidbody body;
 
     // Use this for initialization
     void Start()
     {
     }
-    /*
-    void OnTriggerEnter(Collider other)
-    {
-        if (heldWeapon != null || !other.gameObject.CompareTag("Weapon"))
-        {
-            return;
-        }
-
-        heldWeapon = other.gameObject;
-    }
-
-    /*
-     void OnTriggerExit(Collider other)
-    {
-        if (heldWeapon!=null&&other.gameObject.CompareTag("Weapon")&&other.gameObject.Equals(heldWeapon))
-        {
-            heldWeapon = null;
-        }
-    }*/
 
     // Update is called once per frame
     void Update()
     {
         var device = SteamVR_Controller.Input((int)trackedObject.index);
-        if (/*body != null && */heldWeapon != null && device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        if (heldWeapon != null && device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
         {
             FireArm arm = heldWeapon.GetComponent<FireArm>();
             arm.Fire();
             device.TriggerHapticPulse(arm.PulseDuration());
         }
-        /*else if (body == null && heldWeapon != null && device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
-        {
-            body = heldWeapon.GetComponent<Rigidbody>();
-            body.transform.position = transform.position;
-            body.useGravity = false;
-            body.isKinematic = true;
-            body.transform.SetParent(transform);
-            joint.connectedBody = body;
-            body.isKinematic = true;
-            body.detectCollisions = true;
-
-        }
-        else if (body != null && heldWeapon != null && device.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
-        {
-            body.velocity = device.velocity;
-            body.angularVelocity = device.angularVelocity;
-            body.useGravity = true;
-            body.isKinematic = false;
-            body.transform.parent = null;
-            body = null;
-            joint.connectedBody = null;
-            heldWeapon = null;
-        }*/
     }
 }
